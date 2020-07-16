3c248727e38fdc776079b3f0030597e2d5cba77b
dhanalakshmi5k9@gmail.com <53445441+Kothadhana@users.noreply.github.com>
2020-07-16 11:15:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+$("#img1").on("click",function(){
+  window.open("http://www.aquashipping.co.in/");
+});
+
+$("#img3").on("click",function(){
+  window.open("http://hardipshipping.com/");
+});
+$("#img4").on("click",function(){
+  window.open("https://www.kohliwebdesign.com/");
+});
+$("#img5").on("click",function(){
+  window.open("http://paragwealth.com/");
+});
